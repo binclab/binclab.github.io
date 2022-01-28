2da963e738ed14e5286e31d0975f69680a48f873
bretantonio <bretjoseph@gmail.com>
2022-01-28 12:08:19 +0200

    Update

--- js/facebook.js
+++ js/facebook.js
@@ -1,4 +1,3 @@
-<script>
     window.fbAsyncInit = function() {
         FB.init({
             appId: '310224639988557',
@@ -7,19 +6,16 @@
             version: 'v12.0'
         });
     };
-</script>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
-
-// Facebook login with JavaScript SDK
-function fbLogin() { 
-  FB.login(function(response) { 
-    if (response.authResponse) { 
-      console.log('Welcome! Fetching your information.... '); 
-      FB.api('/me', function(response) { 
-        console.log('Good to see you, ' + response.name + '.');
-      });
-    } else { 
-      console.log('User cancelled login or did not fully authorize.');
+    // Facebook login with JavaScript SDK
+    function fbLogin() {
+        FB.login(function(response) {
+            if (response.authResponse) {
+                console.log('Welcome! Fetching your information.... ');
+                FB.api('/me', function(response) {
+                    console.log('Good to see you, ' + response.name + '.');
+                });
+            } else {
+                console.log('User cancelled login or did not fully authorize.');
+            }
+        });
     }
-  });
-}
